50332f808eaad552766a7aa043cfc8d422cc553b
Vladimir Novopashin <novopashinwm@mail.ru>
2023-10-15 14:00:35 +0300

    Ready for control

--- DZ06_FileReadParallel/DZ06_FileReadParallel/Program.cs
+++ DZ06_FileReadParallel/DZ06_FileReadParallel/Program.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
+
+using static System.Net.Mime.MediaTypeNames;
 
 class Program
 {
     static void Main(string[] args)
     {
+      
         Console.WriteLine("Read 3 files");
         CountSpaceInThreeFiles();
         Console.WriteLine(new string('=',80));
         Console.WriteLine(Environment.NewLine + Environment.NewLine + Environment.NewLine);
 
         Console.WriteLine("ReadFolder");
-        CountSpacesInFolder("folder");
+        CountSpacesInFolder("FILES");
         Console.ReadKey();
     }
 
@@ -45,13 +49,19 @@ class Program
 
     private static void CountSpacesInFolder(string folderPath)
     {
-        string[] fileNames = Directory.GetFiles(folderPath);
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        string[] fileNames = Directory
+                .EnumerateFiles(folderPath, "*", SearchOption.AllDirectories)
+                .Select(Path.GetFileName).ToArray();
+
         Task<int>[] tasks = new Task<int>[fileNames.Length];
 
         for (int i = 0; i < fileNames.Length; i++)
         {
             string fileName = fileNames[i];
-            tasks[i] = Task.Run(() => CountSpacesInFile(fileName));
+            tasks[i] = Task.Run(() => CountSpacesInFile(fileName, folderPath));
         }
 
         Task.WaitAll(tasks);
@@ -60,12 +70,15 @@ class Program
         {
             Console.WriteLine("File {0}: {1} spaces", Path.GetFileName(fileNames[i]), tasks[i].Result);
         }
+        stopwatch.Stop();
+
+        Console.WriteLine("Execution time: {0}", stopwatch.Elapsed);
     }
 
-    private static int CountSpacesInFile(string fileName)
+    private static int CountSpacesInFile(string fileName , string folder = "FILES")
     {
         int count = 0;
-        using (StreamReader reader = new StreamReader(fileName))
+        using (StreamReader reader = new StreamReader(folder + "\\" + fileName))
         {
             string line;
             while ((line = reader.ReadLine()) != null)
